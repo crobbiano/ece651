e65586397e7070f156014193baf91f4c395653e8
Chris <cp.robbiano@gmail.com>
2017-10-15 19:34:54 -0600

    Working with results in notebook.  Need to add knobs for chaning parameters.

--- compass1/compass1.m
+++ compass1/compass1.m
@@ -4,7 +4,8 @@ clear all
 clc
 %% Read in test images
 cd('images\cropped');
-ts = dir('cropped*.png');
+% ts = dir('test*.png');
+ts = dir('Image*.png');
 for i=1:length(ts)
 %     images(i) = load(ts(i).name);
     images{i} = imread(ts(i).name);
@@ -40,50 +41,56 @@ sigWhite=zeros(2025,12);
 for i=1:12
     sigWhite(:,i) = D*sig(i,:)';
     subplot(4,3,i);
-    imshow(reshape(sigWhite(:,i),45,45),[])
+%     imshow(reshape(sigWhite(:,i),45,45),[])
 end
 
 
 %% Look at just the first image for now and scan windows for tumors
 %  for each window, check the test statistics and determine what hypothesis
 %  should be accepted
-currImage = images{1};
+% currImage = images(1).Image;
+currImage = images{6};
 % Cheat and test against a signal
-% currImage = reshape(sig(7,:),45,45);
+% currImage = reshape(sig(6,:),45,45);
 
 detects = zeros(1,12);
-figure(234);clf;
-for i=1:2:size(currImage,1)-windowSize+1
-    for j=1:2:size(currImage,2)-windowSize+1
-%         window = D*(reshape(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),2025,1));
-        window = D*(reshape(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),2025,1)-mu');
+plotThings = 0;
+if (plotThings)
+    figure(234);clf;
+end
+coords=[];
+for i=1:1:size(currImage,1)-windowSize+1
+    for j=1:1:size(currImage,2)-windowSize+1
+        window = D*(reshape(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),2025,1));
+%         window = D*(reshape(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),2025,1)-mu');
         
-%         window = reshape(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),[],1)-mu;
+%         window = reshape(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),[],1)-mu';
 %         hist(window,255);
         
         tests = [];
-        for l=1:12
-%             window'*inv(C)*reshape(sig(:,l),45,45)
-%             tests(l)=window'*sig(:,l) - .5*sig(:,l)'*sig(:,l);
-%             tests(l)=window'*sig(:,l);
-            tests(l)=window'*sigWhite(:,l)- .5*sigWhite(:,l)'*sigWhite(:,l);
-%             tests(l)=reshape(window,1,[])*sigWhite(:,l);
-%             tests(l)=reshape(window,1,[])*sigWhite(:,l) - .5*sigWhite(:,l)'*sigWhite(:,l);
+        for l=1:4
+%             tests(l)=window'*sig(l,:)' - .5*sig(l,:)*sig(l,:)';
+            tests(l)=window'*sigWhite(:,l) - .5*sigWhite(:,l)'*sigWhite(:,l);
         end
         [maxim,idx] = max(tests);
+        if (idx ~= 1)
+            coords(end+1,:) = [ (j+windowSize/2) (i+windowSize/2) idx];
+        end
 %         display(['max: ' num2str(idx)])
         detects(idx) = detects(idx) + 1;
-        subplot(1,3,1)
-%         imshow(D*double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),[]);
-        imshow(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1)))-reshape(mu,45,45),[]);
-        xlabel(['(' num2str(i) ',' num2str(j) ')'])
-        subplot(1,3,2)
-        imshow(reshape(sigWhite(:,idx),45,45),[]);
-        xlabel(['signal: ' num2str(idx)])
-        subplot(1,3,3)
-        imshow(reshape(window,45,45),[]);
-        xlabel('window')
-%         pause(.01)
+        if (plotThings)
+            subplot(1,3,1)
+            %         imshow(D*double(currImage(i:windowSize+(i-1),j:windowSize+(j-1))),[]);
+            imshow(double(currImage(i:windowSize+(i-1),j:windowSize+(j-1)))-reshape(mu,45,45),[]);
+            xlabel(['(' num2str(j+windowSize/2) ',' num2str(i+windowSize/2) ')'])
+            subplot(1,3,2)
+            imshow(reshape(sigWhite(:,idx),45,45),[]);
+            xlabel(['signal: ' num2str(idx)])
+            subplot(1,3,3)
+            imshow(reshape(window,45,45),[]);
+            xlabel('window')
+            pause(.0001)
+        end
     end
     1;
 end
