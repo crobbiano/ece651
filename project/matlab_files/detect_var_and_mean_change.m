ba1e39ca2d19cf35d90d9330439262a1ee6b1dc7
Chris <iamchris@rams.colostate.edu>
2017-12-04 15:26:16 -0700

    Minor changes. Need to implement the actual test statistics that were derived in notes for the mean detection

--- project/matlab_files/detect_var_and_mean_change.m
+++ project/matlab_files/detect_var_and_mean_change.m
@@ -5,32 +5,35 @@ clc
 f0 = .1;
 n=1:1000;
 % signal_snr = 20;
-vars = .01;
+vars = .1;
 signal_snr = -10*log10(vars)
 
 % true_signal = cos(f0.*n);
-true_signal = gen_ux0(length(n), 10);
+true_signal = gen_ux0(length(n), 2);
 noisy_true_signal = awgn(true_signal, signal_snr);
-truevar = var(noisy_true_signal-true_signal);
+% truevar = var(noisy_true_signal-true_signal);
 truevar = vars;
 
-var_offset = .1;
-mean_offset = 2;
+var_offset = .0;
+mean_offset = .3;
 mod_snr = -10*log10(truevar + var_offset)
 mod_signal = true_signal;
 mod_signal(1:99) = awgn(true_signal(1:99), signal_snr);
 mod_signal(100:199) = awgn(true_signal(100:199), mod_snr);
-mod_signal(200:end) = awgn(true_signal(200:end), mod_snr) + linspace(0,mean_offset,length(n) - 200 + 1);
+mod_signal(200:end) = awgn(true_signal(200:end), mod_snr) + mean_offset;
+% mod_signal(200:end) = awgn(true_signal(200:end), mod_snr) + linspace(0,mean_offset,length(n) - 200 + 1);
 x = mod_signal;
 modvar = truevar + var_offset
 change_points = [100 200];
 
 alpha = [-10:.01:10]';
-mean_thresh = 1.3*sqrt(truevar);
+mean_thresh = 3*sqrt(truevar);
 
-beta = [-truevar+.0001:.001:2*truevar]';
-var_thresh = (truevar)/1;
-var_thresh2 = truevar*8;
+% beta = [-truevar+.0001:.001:2*truevar]';
+beta = [0:.001:2*truevar]';
+var_thresh = (truevar)*1.5;
+% var_thresh2 = truevar*8;
+var_thresh2 = sqrt(truevar)*2;
 
 alarm_window_sz = 30; % size of window to count alarm detections in
 alarm_window_sz2 = 20; % size of window to count alarm detections in
@@ -67,7 +70,8 @@ for t=n % This is the time index
     
         % calculate the likelihood of x(n) with different mean offsets
     for a=1:length(alpha);
-        xlikelihood_m(a) = (1/sqrt(2*pi*truevar))*exp(-(1/(2*truevar))*(x(t) + alpha(a) - true_signal(t))^2);
+%         xlikelihood_m(a) = (1/sqrt(2*pi*truevar))*exp(-(1/(2*truevar))*(x(t) + alpha(a) - true_signal(t))^2);
+        xlikelihood_m(a) = (1/sqrt(2*pi*truevar))*exp(-(1/(2*truevar))*(x(t) - alpha(a))^2);
     end
     
     % find the mean offset that produced the greatest likelihood
@@ -77,6 +81,7 @@ for t=n % This is the time index
     
     % compare the mean offset to the mean threshold
     if (abs(detected_mu) > mean_thresh )
+%     if (abs(detected_mu) > 1.5*abs(true_signal(t)) )
         if (t ~= alarm_time_m + alarm_window_idx_m)
             alarm_window_idx_m = 1;
             alarm_time_m = t;
@@ -87,17 +92,18 @@ for t=n % This is the time index
     end
     % count the number of alarms in the past alarm_window_sz and flag
     % if more than the threshold
-    alarm_thresh_m = alarm_window_sz;
+    alarm_thresh_m = alarm_window_sz/2;
     num_alarms_m = sum(alarms_m > t - alarm_window_sz);
     if (num_alarms_m >= alarm_thresh_m)
         final_time = alarm_time_m - change_points(2);
         found_mu = 1;
-        display(['Found mean'])
+        display(['Found mean of ' num2str(detected_mu) '; true = ' num2str(true_signal(t))])
     end
     
     % calculate the likelihood of x(n) with different mean offsets
     for b=1:length(beta);
-        xlikelihood(b) = (1/sqrt(2*pi*(truevar + beta(b))))*exp(-(1/(2*(truevar + beta(b))))*(x(t) - true_signal(t))^2);
+%         xlikelihood(b) = (1/sqrt(2*pi*(truevar + beta(b))))*exp(-(1/(2*(truevar + beta(b))))*(x(t) - true_signal(t))^2);
+        xlikelihood(b) = (1/sqrt(2*pi*(beta(b))))*exp(-(1/(2*(beta(b))))*(x(t) - true_signal(t))^2);
     end
     
     % find the mean offset that produced the greatest likelihood
@@ -153,8 +159,10 @@ end
 %% plot things
 figure(10);clf;hold on
 plot(n,x,'-',n,true_signal,'--', change_points, x(change_points), 'ko', alarms, zeros(length(alarms)),'xr',alarms_m, zeros(length(alarms_m)),'ob')
+plot(n,true_signal+sqrt(var_thresh),'--.k',n,true_signal-sqrt(var_thresh),'--.k')
 line([t t],ylim,'Color',[1,0,0])
 for i=1:length(change_points)
     line([change_points(i) change_points(i)],ylim,'Color',[0,0,0],'LineStyle','--')
 end
 xlim([0 t+10])
+grid minor
